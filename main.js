e445df922a04efb6cdd6bdf3946c57c4e164e597
William Spigner <senditomy@gmail.com>
2020-06-11 15:00:31 -0700

    update

---
+++ main.js
@@ -0,0 +1,69 @@
+var inquirer = require('inquirer');
+
+
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project's name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What command should be run to install dependencies?",
+    default: "npm i",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+    default: "npm test",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What does the user need to know about contributing to the repo?",
+  },
+];
+
+inquirer
+  .prompt(
+    /* Pass your questions in here */
+    questions
+  )
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
+
+function writeToFile(fileName, data) {}
+
+function init() {}
+init();
